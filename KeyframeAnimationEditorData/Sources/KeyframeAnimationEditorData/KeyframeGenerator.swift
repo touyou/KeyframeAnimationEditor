f89558696ef60c2636e1d80cc074aa34e3e78969
touyou <fujiyou1121@gmail.com>
2023-06-29 14:39:02 +0900

    implement generator

--- KeyframeAnimationEditorData/Sources/KeyframeAnimationEditorData/KeyframeGenerator.swift
+++ KeyframeAnimationEditorData/Sources/KeyframeAnimationEditorData/KeyframeGenerator.swift
@@ -7,20 +7,16 @@
 
 import SwiftUI
 
-public struct KeyframeGenerator<Root, Value, Content> where Value: Animatable, Content: KeyframeTrackContent, Value == Content.Value {
+public struct KeyframeGenerator<Root, Value> where Value: Animatable {
     public let path: WritableKeyPath<Root, Value>
     
-    public var keyframeObjects: Array<KeyframeObject<Value>>
-    public var keyframeTrack: KeyframeTrack<Root, Value, Content> {
+    public var keyframeObjects: [KeyframeObject<Value>]
+    public var keyframeTrack: KeyframeTrack<Root, Value, some KeyframeTrackContent<Value>> {
         KeyframeTrack(path) {
-            KeyframeTrackContentBuilder<Value>.buildArray(keyframes)
+            KeyframesBuilder.buildArray(keyframeObjects.map { $0.keyframe })
         }
     }
-    
-    var keyframes: [Content] {
-        keyframeObjects.map { $0.getKeyframe() as! Content }
-    }
-    
+
     public init(path: WritableKeyPath<Root, Value>, keyframeObjects: Array<KeyframeObject<Value>>) {
         self.path = path
         self.keyframeObjects = keyframeObjects
--- KeyframeAnimationEditorData/Sources/KeyframeAnimationEditorData/KeyframeObject.swift
+++ KeyframeAnimationEditorData/Sources/KeyframeAnimationEditorData/KeyframeObject.swift
@@ -12,14 +12,16 @@ public struct KeyframeObject<Value> where Value: Animatable {
     let to: Value
     let duration: Double?
     
-    public func getKeyframe() -> any KeyframeTrackContent {
+    // Spring, Linear, Move, Cubic
+    @KeyframeTrackContentBuilder<Value>
+    var keyframe: some KeyframeTrackContent<Value> {
         if type == "spring" {
             SpringKeyframe(to, duration: duration)
         } else {
             CubicKeyframe(to, duration: duration ?? 0.5)
         }
     }
-    
+        
     public init(to: Value, duration: Double?) {
         self.to = to
         self.duration = duration
